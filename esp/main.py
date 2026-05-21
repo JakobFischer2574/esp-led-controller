c2ce003183ec30558c8e277822fe2003d1a0e928
Jakob Fischer <jako.fischer@outlook.de>
2026-05-21 14:53:21 +0200

    Bump version to 0.2.4 in manifest.json and update update check interval to 60 seconds

--- esp/main.py
+++ esp/main.py
@@ -14,7 +14,7 @@ except Exception as e:
 LED_PINS = [14, 27, 25, 32, 33]
 
 # Update-Check alle 60 Sekunden
-UPDATE_INTERVAL_MS = 10_000
+UPDATE_INTERVAL_MS = 60_000
 
 leds = []
 led_states = [0, 0, 0, 0, 0]
